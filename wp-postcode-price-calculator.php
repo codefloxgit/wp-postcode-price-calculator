367bffc3853044701c8dec16012363ce385cd5f5
Manoj Singh <manojsinghrwt@hotmail.com>
2018-08-13 13:23:09 +0530

    scripts files added
    php,js,css files added

--- wp-postcode-price-calculator.php
+++ wp-postcode-price-calculator.php
@@ -24,21 +24,34 @@ if(!defined('ABSPATH')){
 }
 define("PCPC_PLUGIN_DIR",plugin_dir_url(__FILE__));
 define("PCPC_PLUGIN_PATH",plugin_dir_path(__FILE__));
-require_once('includes/api.php');
-require('includes/Postcodes-IO-PHP.php');
+require_once('includes/wppostcode.php');
+require('includes/postcodes-io-api.php');
 class PCPC_Init{
 	public static function init(){
         $class = __CLASS__;
         new $class;
     }
-	public function __construct(){}
+	public function __construct(){
+		add_action('wp_head',array($this,'wppcpc_object'));
+		add_action('wp_enqueue_scripts',array($this,'js'));
+		add_action('wp_enqueue_scripts',array($this,'css'));
+		#add_action('admin_enqueue_scripts',array($this,'js'));
+		#add_action('admin_enqueue_scripts',array($this,'css'));
+	}
 	public function css(){
 		wp_register_style('pcpccss',PCPC_PLUGIN_DIR.'css/pcpc.css');
 		wp_enqueue_style('pcpccss');
+		wp_register_style('sweetalert2css',PCPC_PLUGIN_DIR.'css/sweetalert2.min.css',false,'7.24.3');
+    	wp_enqueue_style('sweetalert2css' );
 	}
 	public function js(){
+		wp_register_script('sweetalert2js',PCPC_PLUGIN_DIR.'js/sweetalert2.min.js',array('jquery'),'7.24.3',true);
+		wp_enqueue_script('sweetalert2js');
 		wp_register_script('pcpcjs',PCPC_PLUGIN_DIR.'js/pcpc.js',array('jquery'),'0.0.1',true);
 		wp_enqueue_script('pcpcjs');
+	}
+	public function wppcpc_object(){
+		?><script>var wppcpcobj={"ajaxurl":"<?php echo admin_url('admin-ajax.php'); ?>"}</script><?php
 	}
 }
 add_action('plugins_loaded',array('PCPC_Init','init'));
